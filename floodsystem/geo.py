4bdcf6ef0dad98ba15dce91694bc234b978408f3
CalumStevenson <crbs2@cam.ac.uk>
2022-01-23 13:11:18 +0000

    Task 1E Complete

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -23,3 +23,20 @@ def stations_by_river(stations):
     for station in stations:
         river_dictionary[station.river].append(station.name)
     return river_dictionary
+
+def rivers_by_station_number(stations,N):
+    riverList = stations_by_river(stations)
+    riverStationCount = []
+    for river in riverList:
+        riverStationCount.append((river,len(riverList[river])))
+    riverStationCount.sort(key = lambda x:x[1])
+    riverStationCount.reverse()
+    if N>0:
+        outputList = riverStationCount[0:N]
+        count = 0
+        if len(outputList) < len(riverStationCount):
+            while outputList[len(outputList)-1][1] == riverStationCount[N][1]+count:
+                outputList.append(riverStationCount[N+count])
+                count +=1
+        return outputList
+    return None
