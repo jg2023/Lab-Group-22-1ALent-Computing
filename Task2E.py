54bcdb5ecc313e02157c71f51a1e80d0cd2fcc00
CalumStevenson <crbs2@cam.ac.uk>
2022-01-29 17:39:17 +0000

    More work on 2E

--- Task2E.py
+++ Task2E.py
@@ -1,29 +1,48 @@
 from floodsystem.datafetcher import fetch_measure_levels
 from floodsystem.stationdata import build_station_list
+from floodsystem.plot import plot_water_levels
 import datetime
-
+from tqdm import tqdm
 
 stations = build_station_list()
 
 
 
-
+"""
 topTenStations = []
-for station in stations:
-    print(station)
+for station in tqdm(stations, desc = "Loading: "):
     dt = 2
-    dates, levels = fetch_measure_levels(
-    station.measure_id, dt=datetime.timedelta(days=dt))
-    station.latest_level = levels[0]
+    dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
+    count = 0
     try:
-        for i in range(len(topTenStations)):
-            if station.latest_level - station.average_value > topTenStations[i].latest_level - topTenStations[i].average_value:
-                topTenStations.insert(station)
-                topTenStations.pop(len(topTenStations))
-                break
-
-        
+        station.latest_level = levels[0]
     except:
-        topTenStations.append(station)
+        if type(levels) == int:
+            station.latest_level = levels
+        elif type(levels) == str:
+            if levels.isdigit():
+                station.latest_level = int(levels)
+        else:
+            station.latest_level = station.average_value
+
+
+    if len(topTenStations) == 0:
+        topTenStations.append((station,dates,levels))
+    else:
+        for i in range(len(topTenStations)):
+            if station.latest_level - station.average_value > topTenStations[i][0].latest_level - topTenStations[i][0].average_value:
+                topTenStations.insert(i,(station,dates,levels))
+                count = 1
+                break
+        if len(topTenStations) <= 10 and count == 0:
+            topTenStations.append((station,dates,levels))
+        elif len(topTenStations)> 10:
+            topTenStations.pop(len(topTenStations)-1)
+
 for station in topTenStations:
-    print(station.name,station.level)
+    plot.plot_water_levels(station[0],stations[1],station[2])
+    """
+station = stations[0]
+dt = 2
+dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
+plot_water_levels(station,dates,levels)
--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -2,10 +2,10 @@ import matplotlib.pyplot as plt
 
 def plot_water_levels(station,dates,levels):
     """This procedure plots water level against time for a certain station"""
-    plt.plot(dates,levels)
+    plt.plot(dates,levels , label = 'Water Level Against Time')
     plt.xlabel('date')
     plt.ylabel('water level (m)')
-    plt.xticks(rotation=45);
+    plt.xticks(rotation=45)
     plt.title(station.name)
-    plt.tight_layout() 
+    plt.legend()
     plt.show
